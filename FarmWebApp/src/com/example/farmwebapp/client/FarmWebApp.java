0b913af7ced147c6661ef7e2be229e8ea39f8750
Russell Tan <sell_nat@yahoo.com>
2015-02-08 15:56:20 -0800

    Buttons now being displyed on root page

--- FarmWebApp/src/com/example/farmwebapp/client/FarmWebApp.java
+++ FarmWebApp/src/com/example/farmwebapp/client/FarmWebApp.java
@@ -2,26 +2,18 @@ package com.example.farmwebapp.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
 
 public class FarmWebApp implements EntryPoint
 {
-
-	@Override
+	
 	public void onModuleLoad() 
 	{
-
+		Home homePage = new Home();
 		
-		Button myButton = new Button("Hello");
-		
-		RootPanel.get("helloWorld").add(myButton);
-		RootPanel.get("helloWorld").add(myButton);
-		RootPanel.get("helloWorld").add(myButton);
-		RootPanel.get("helloWorld").add(myButton);
-		RootPanel.get("helloWorld").add(myButton);
-		RootPanel.get("helloWorld").add(myButton);
+		RootPanel.get("homePage").add(homePage.getPanel());		
 	}
+	
 	
 }
--- FarmWebApp/src/com/example/farmwebapp/client/Home.java
+++ FarmWebApp/src/com/example/farmwebapp/client/Home.java
@@ -1,23 +1,29 @@
-/**
- * 
- */
-
 package com.example.farmwebapp.client;
 
 import com.google.gwt.user.client.ui.*;
 
 public class Home 
 {
-	private HorizontalPanel pHome;
-	private VerticalPanel pLogo;
-	private VerticalPanel pButtons;
 	
 	
-	
-	
-	
-	public HorizontalPanel getPanel()
+	public VerticalPanel getPanel()
 	{
+		VerticalPanel pHome = new  VerticalPanel();
+		//private VerticalPanel pLogo;
+		VerticalPanel pButtons = new VerticalPanel();
+		Button bNewPatient = new Button("New Patient");
+		Button bSearchPatients = new Button("Search Patients");
+		Button bSettings = new Button("Settings");
+		//private Button bUpdates = new Button("Updates");
+		Button bLogout = new Button("Logout");
+		
+		pButtons.add(bNewPatient);
+		pButtons.add(bSearchPatients);
+		pButtons.add(bSettings);
+		pButtons.add(bLogout);
+		
+		pHome.add(pButtons);
+
 		return pHome;
 	}
 	
