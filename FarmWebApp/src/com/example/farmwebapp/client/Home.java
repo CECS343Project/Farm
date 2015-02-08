7917ccc8a8a68d30dbb7374387a7018be6d50c6a
Russell Tan <sell_nat@yahoo.com>
2015-02-08 14:28:41 -0800

    Working on home gui getpanel

--- FarmWebApp/src/com/example/farmwebapp/client/Home.java
+++ FarmWebApp/src/com/example/farmwebapp/client/Home.java
@@ -1,11 +1,24 @@
+/**
+ * 
+ */
+
 package com.example.farmwebapp.client;
+
+import com.google.gwt.user.client.ui.*;
 
 public class Home 
 {
+	private HorizontalPanel pHome;
+	private VerticalPanel pLogo;
+	private VerticalPanel pButtons;
 	
-	public void getPanel()
+	
+	
+	
+	
+	public HorizontalPanel getPanel()
 	{
-		
+		return pHome;
 	}
 	
 	public void removePanel()
