63b11abfac76dc312c6203ee371edf6ba30b416a
Napoleon Fulinara Jr <nfulinarajr@users.noreply.github.com>
2015-02-08 13:11:05 -0800

    Took out MGWT files

--- FarmWebApp/src/com/example/farmwebapp/client/FarmWebApp.java
+++ FarmWebApp/src/com/example/farmwebapp/client/FarmWebApp.java
@@ -4,8 +4,6 @@ package com.example.farmwebapp.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.googlecode.mgwt.ui.client.MGWT;
-import com.googlecode.mgwt.ui.client.MGWTSettings;
 
 
 public class FarmWebApp implements EntryPoint
@@ -14,7 +12,7 @@ public class FarmWebApp implements EntryPoint
 	@Override
 	public void onModuleLoad() 
 	{
-		MGWT.applySettings(MGWTSettings.getAppSetting());
+
 		
 		Button myButton = new Button("Hello");
 		
