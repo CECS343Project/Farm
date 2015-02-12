3ce27e4a428d88b73c9df3f87414515c09bffbaf
Russell Tan <sell_nat@yahoo.com>
2015-02-12 08:47:08 -0800

    Removed servlet from build path and removed unused imports

--- FarmWebApp/src/com/example/farmwebapp/client/FarmWebApp.java
+++ FarmWebApp/src/com/example/farmwebapp/client/FarmWebApp.java
@@ -1,7 +1,6 @@
 package com.example.farmwebapp.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.*;
 
 
--- FarmWebApp/src/com/example/farmwebapp/client/Login.java
+++ FarmWebApp/src/com/example/farmwebapp/client/Login.java
@@ -1,5 +1,4 @@
 package com.example.farmwebapp.client;
-import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
