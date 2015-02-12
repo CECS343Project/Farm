5ff05eae815949372a7150972a640a818fbfc9b6
Russell Tan <sell_nat@yahoo.com>
2015-02-12 10:04:02 -0800

    Fixed DOB in flextable

--- FarmWebApp/src/com/example/farmwebapp/client/NewPatient.java
+++ FarmWebApp/src/com/example/farmwebapp/client/NewPatient.java
@@ -7,6 +7,7 @@ public class NewPatient
 	public VerticalPanel getPanel()
 	{
 		VerticalPanel pNewPatientMain = new VerticalPanel();
+		HorizontalPanel pDOB = new HorizontalPanel();
 		FlexTable ftPatientInfo = new FlexTable();
 		
 		TextBox tbFName = new TextBox();
@@ -17,22 +18,19 @@ public class NewPatient
 		TextBox tbEmail = new TextBox();
 		TextBox tbHPhone = new TextBox();
 		TextBox tbCPhone = new TextBox();
-		TextBox tbDOB = new TextBox();
+		ListBox lbDOBDay = new ListBox();
+		ListBox lbDOBMonth = new ListBox();
+		ListBox lbDOBYear = new ListBox();
 		TextBox tbPolicyNo = new TextBox();
 		TextBox tbPolicyIssuer = new TextBox();
 		TextBox tbPolicyStatus = new TextBox();
-		
 		TextBox tbMedicine = new TextBox();
 		TextBox tbDose = new TextBox();
 		TextBox tbAllergies = new TextBox();
-		
-		TextBox tbFrequency = new TextBox();
+		ListBox lbFrequency = new ListBox();
 		TextBox tbMedStartDate = new TextBox();
 		TextBox tbMedEndDate = new TextBox();
-		
-		TextArea taNotes = new TextArea();
-		
-		
+		TextArea taNotes = new TextArea();		
 		
 		Label lFName = new Label("First Name:");
 		Label lLName = new Label("Last Name:");
@@ -52,57 +50,47 @@ public class NewPatient
 		Label lFrequency = new Label("Frequency:");
 		Label lMedStartDate = new Label("Start Date:");
 		Label lMedEndDate = new Label("End Date:");
-		
-		Label lNotes = new Label("Doctors Notes:");			
-		
-		ftPatientInfo.setWidget(0, 1, tbFName);
-		ftPatientInfo.setWidget(0, 3, tbMName);
-		ftPatientInfo.setWidget(0, 5, tbLName);
-		ftPatientInfo.setWidget(0, 7, tbSuffix);
-		ftPatientInfo.setWidget(1, 1, tbAddress);
-		ftPatientInfo.setWidget(2, 1, tbEmail);
-		
-		ftPatientInfo.setWidget(3, 1, tbHPhone);
-		ftPatientInfo.setWidget(3, 3, tbCPhone);
-		ftPatientInfo.setWidget(4, 1, tbDOB);
-		ftPatientInfo.setWidget(5, 1, tbPolicyNo);
-		ftPatientInfo.setWidget(5, 3, tbPolicyIssuer);
-		
-		ftPatientInfo.setWidget(5, 5, tbPolicyStatus);
-		ftPatientInfo.setWidget(6, 1, tbMedicine);
-		ftPatientInfo.setWidget(6, 3, tbDose);
-		ftPatientInfo.setWidget(6, 5, tbFrequency);
-		ftPatientInfo.setWidget(7, 1, tbAllergies);
-		
-		ftPatientInfo.setWidget(8, 1, tbMedStartDate);
-		ftPatientInfo.setWidget(8, 2, tbMedEndDate);
-		
-		pNewPatientMain.add(ftPatientInfo);
-		pNewPatientMain.add(taNotes);
-
-		
+		Label lNotes = new Label("Doctors Notes:");	
+		pDOB.add(lbDOBDay);
+		pDOB.add(lbDOBMonth);
+		pDOB.add(lbDOBYear);
 		
 		ftPatientInfo.setWidget(0, 0, lFName);
+		ftPatientInfo.setWidget(0, 1, tbFName);
 		ftPatientInfo.setWidget(0, 2, lMName);
+		ftPatientInfo.setWidget(0, 3, tbMName);
 		ftPatientInfo.setWidget(0, 4, lLName);
+		ftPatientInfo.setWidget(0, 5, tbLName);
 		ftPatientInfo.setWidget(0, 6, lSuffix);
+		ftPatientInfo.setWidget(0, 7, tbSuffix);
 		ftPatientInfo.setWidget(1, 0, lAddress);
+		ftPatientInfo.setWidget(1, 1, tbAddress);
 		ftPatientInfo.setWidget(2, 0, lEmail);
-		
+		ftPatientInfo.setWidget(2, 1, tbEmail);
 		ftPatientInfo.setWidget(3, 0, lHPhone);
+		ftPatientInfo.setWidget(3, 1, tbHPhone);
 		ftPatientInfo.setWidget(3, 2, lCPhone);
+		ftPatientInfo.setWidget(3, 3, tbCPhone);
 		ftPatientInfo.setWidget(4, 0, lDOB);
+		ftPatientInfo.setWidget(4, 1, pDOB);
 		ftPatientInfo.setWidget(5, 0, lPolicyNo);
+		ftPatientInfo.setWidget(5, 1, tbPolicyNo);
 		ftPatientInfo.setWidget(5, 2, lPolicyIssuer);
-		
+		ftPatientInfo.setWidget(5, 3, tbPolicyIssuer);
 		ftPatientInfo.setWidget(5, 4, lPolicyStatus);
+		ftPatientInfo.setWidget(5, 5, tbPolicyStatus);
 		ftPatientInfo.setWidget(6, 0, lMedicine);
+		ftPatientInfo.setWidget(6, 1, tbMedicine);
 		ftPatientInfo.setWidget(6, 2, lDose);
+		ftPatientInfo.setWidget(6, 3, tbDose);
 		ftPatientInfo.setWidget(6, 4, lFrequency);
+		ftPatientInfo.setWidget(6, 5, lbFrequency);
 		ftPatientInfo.setWidget(7, 0, lAllergies);
-		
+		ftPatientInfo.setWidget(7, 1, tbAllergies);
 		ftPatientInfo.setWidget(8, 0, lMedStartDate);
-		ftPatientInfo.setWidget(8, 1, lMedEndDate);
+		ftPatientInfo.setWidget(8, 1, tbMedStartDate);
+		ftPatientInfo.setWidget(8, 2, lMedEndDate);
+		ftPatientInfo.setWidget(8, 3, tbMedEndDate);	
 		
 		pNewPatientMain.add(ftPatientInfo);
 		pNewPatientMain.add(lNotes);
@@ -110,8 +98,6 @@ public class NewPatient
 		
 		pNewPatientMain.addStyleName("mainPanel");
 			
-	
-	
 		return pNewPatientMain;
 	}
 
