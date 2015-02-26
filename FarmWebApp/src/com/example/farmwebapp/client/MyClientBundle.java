e931f4f654aae020ee6bb907198a3d96801d7592
Russell Tan <sell_nat@yahoo.com>
2015-02-26 10:25:43 -0800

    Attempt failed

---
+++ FarmWebApp/src/com/example/farmwebapp/client/MyClientBundle.java
@@ -0,0 +1,15 @@
+package com.example.farmwebapp.client;
+
+import org.helios.gwt.fonts.client.FontResource;
+
+import com.google.gwt.resources.client.ClientBundle;
+
+public interface MyClientBundle extends ClientBundle
+{
+	   // be sure to include eot fonts for IE8 and previous versions)
+	 //  @Source({"aqua-webfont.eot", "aqua-webfont.eot?#iefix","aqua-webfont.woff","aqua-webfont.ttf","aqua-webfont.svg#svgFontName"})
+	   //FontResource myFont();
+
+	  // @Source("styles.css")
+	   //MyCssResource css();
+}
