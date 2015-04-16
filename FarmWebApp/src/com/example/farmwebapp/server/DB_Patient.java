7a5a5215b1cea639e6b3c8de588500dd682ea38f
Russell Tan <sell_nat@yahoo.com>
2015-04-15 20:22:20 -0700

    Testing, have solid db connection

--- FarmWebApp/src/com/example/farmwebapp/server/DB_Patient.java
+++ FarmWebApp/src/com/example/farmwebapp/server/DB_Patient.java
@@ -41,14 +41,14 @@ public class DB_Patient extends DB_Conn
 				patientData[k].city = result.getString(5);
 				patientData[k].state = result.getString(6);
 				patientData[k].zip = result.getString(7);
-				patientData[k].dob = result.getDate(8);
-				patientData[k].complaint = result.getString(9);
-				patientData[k].history = result.getString(10);
-				patientData[k].medRecord = result.getString(11);
-				patientData[k].progress = result.getString(12);
-				patientData[k].sugicalHist = result.getString(13);
-				patientData[k].testResult = result.getString(14);
-				patientData[k].treatmentPlan = result.getString(15);
+				patientData[k].dob = result.getDate(10);
+				patientData[k].complaint = result.getString(11);
+				patientData[k].history = result.getString(12);
+				patientData[k].medRecord = result.getString(13);
+				patientData[k].progress = result.getString(14);
+				patientData[k].sugicalHist = result.getString(15);
+				patientData[k].testResult = result.getString(16);
+				patientData[k].treatmentPlan = result.getString(17);
 				
 				k++;
 			}
